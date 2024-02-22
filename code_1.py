a461d8a5cabf707731195c3ade05a6cd1ca4cd85
Alima <a_zhengiskhan@kbtu.kz>
2024-02-22 15:07:07 +0600

    MD

---
+++ code_1.py
@@ -0,0 +1,6 @@
+def Alima(n):
+    return n**2
+    
+m = Alima(3)
+print(m)
+    
---
+++ lab_4/modules.py
@@ -0,0 +1,12 @@
+#1
+import module
+
+#2
+import mymodule as mx
+
+#3
+import mymodule
+print(dir(mymodule()))
+
+#4
+from mymodule import person1
